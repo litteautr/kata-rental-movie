e032d2b1064bad646b408b2925757a1ceb674536
Robin Litteaut <rlitteaut@netseenergy.fr>
2022-12-12 18:21:36 +0100

    set field final

--- src/main/java/movierental/Customer.java
+++ src/main/java/movierental/Customer.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Customer {
 
-  private String name;
-  private List<Rental> rentals = new ArrayList<>();
+  private final String name;
+  private final List<Rental> rentals = new ArrayList<>();
 
   public Customer(String name) {
     this.name = name;
