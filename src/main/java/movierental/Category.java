bdd84afa590541c99bde20c12cfa4cf7470f6e89
Robin Litteaut <robin.litteaut@datanumia.com>
2023-01-04 12:00:48 +0100

    strategy via enum

--- src/main/java/movierental/Category.java
+++ src/main/java/movierental/Category.java
@@ -1,7 +1,17 @@
 package movierental;
 
 public enum Category {
-  REGULAR,
-  NEW_RELEASE,
-  CHILDREN
+  REGULAR(2, 2, 1.5),
+  NEW_RELEASE(0, 0, 3),
+  CHILDREN(1.5, 3, 1.5);
+
+  final double initialAmount;
+  final int daysBeforePenalty;
+  final double penaltyCoefficient;
+
+  Category(double initialAmount, int daysBeforePenalty, double penaltyCoefficient) {
+    this.initialAmount = initialAmount;
+    this.daysBeforePenalty = daysBeforePenalty;
+    this.penaltyCoefficient = penaltyCoefficient;
+  }
 }
--- src/main/java/movierental/Rental.java
+++ src/main/java/movierental/Rental.java
@@ -3,27 +3,11 @@ package movierental;
 public record Rental(Movie movie, int daysRented) {
 
   double amount() {
-    switch (movie.category()) {
-      case REGULAR -> {
-        return calcul(2, 2, 1.5);
-      }
-      case NEW_RELEASE -> {
-        return calcul(0, 0, 3);
-      }
-      case CHILDREN -> {
-        return calcul(1.5, 3, 1.5);
-      }
-      default -> {
-        return 0;
-      }
-    }
-  }
-
-  private double calcul(double initialAmount, double daysBeforePenalty, double penaltyCoefficient) {
-    if (daysRented > daysBeforePenalty) {
-      return initialAmount + (daysRented - daysBeforePenalty) * penaltyCoefficient;
+    Category category = movie.category();
+    if (daysRented > category.daysBeforePenalty) {
+      return category.initialAmount + (daysRented - category.daysBeforePenalty) * category.penaltyCoefficient;
     }
 
-    return initialAmount;
+    return category.initialAmount;
   }
 }
